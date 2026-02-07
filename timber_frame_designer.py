Incorrect number of plates calculation
Plates kept randomly being odd numbers which is not correct. I still have not fixed it but this fixes the odd number issue. Now the issue is the dimensions of the plates are not correct, there is a rounding error in this upload.

timber_frame_designer.py
@@ -155,8 +155,7 @@ MATERIAL REQUIREMENTS (Approximate):
   Posts:         {self.num_bents * 2} pieces @ {self.wall_height} ft
   Beams:         {self.num_bents} pieces @ {self.width} ft
   Rafters:       {self.num_bents * 2} pieces @ {(self.width/2) / np.cos(np.arctan(self.roof_pitch)):.1f} ft
-  Plates:        {int((self.length * 2 + self.width * 2) / 16) + 1} pieces @ 16 ft
-
+  Plates:        {int(np.ceil(self.length * 2 / 16))} pieces @ 16 ft (both walls)
 NOTE: This is a preliminary design. Consult a licensed structural 
 engineer for final design and local building code compliance.
         """